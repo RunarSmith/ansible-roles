236ecf1d3814b5095ed2b3677dbf36c426b1ad7d
RunarSmith <ewhat@protonmail.com>
2024-07-18 14:27:50 +0200

    fixes for burpsuite, and WSL compatibility

--- roles/customize-browser/files/getburpcert.sh
+++ roles/customize-browser/files/getburpcert.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-/bin/bash -c "timeout 45 /usr/lib/jvm/java-17-openjdk-amd64/bin/java -Djava.awt.headless=true -jar /usr/share/burpsuite/burpsuite.jar < <(echo y) &" 
+
+# https://portswigger.net/burp/documentation/desktop/external-browser-config/certificate/ca-cert-firefox
+# we have to start BS, wait for it to be started, and download the cacert
+
+rm -f /tmp/cacert.der
+
+/bin/bash -c "timeout 35 /usr/share/burpsuite/jre/bin/java -Djava.awt.headless=true -jar /usr/share/burpsuite/burpsuite_community.jar < <(echo y) &" 
 sleep 30
-curl http://localhost:8080/cert -o /tmp/cacert.der
+
+curl -s http://localhost:8080/cert -o /tmp/cacert.der
+
 exit
